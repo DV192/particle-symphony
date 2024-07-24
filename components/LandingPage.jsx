61a2a49ee3d6f2d92b9e158a9007792dd6e9d906
Varun Patel <varunvarshil111@gmail.com>
2024-07-24 17:29:34 -0400

    Canvas Changes

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -1,22 +1,46 @@
 "use client";
-import { Scroll, ScrollControls } from '@react-three/drei';
+import { PresentationControls, Scroll, ScrollControls, Sparkles } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import * as THREE from "three";
 import AllSections from './AllSections';
+import ParticleCanvas from './ParticleCanvas';
 
 const LandingPage = () => {
   return (
     <div className='w-full h-full bg-transparent fixed top-0 bottom-0 left-0 right-0 z-20'>
+      <div className='fixed top-0 left-0 bottom-0 right-0 z-10'
+        style={{background: 'linear-gradient(to bottom, rgba(82, 82, 82, 0) 0%, rgba(0, 0, 0, 0.2) 100%)'}}
+      ></div>
       <Canvas>
-        <ambientLight intensity={0.5} color={new THREE.Color('#ffffff')} />
-        <directionalLight intensity={0.1} color={new THREE.Color('#ffffff')} position={[0.25, 0.25, 0.25]} />
-        <spotLight position={[0, 2, 0]} angle={10.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
+        <ambientLight intensity={1.5} />
+        <directionalLight intensity={10} position={[1.0, 0, -1]} />
+        <spotLight position={[0, 4, 4]} intensity={200} distance={15} angle={0.5} penumbra={1} decay={1} />
 
-        <ScrollControls pages={5} damping={0.25}>
+        <ScrollControls pages={5} damping={0.25} style={{zIndex: '20'}}>
+          <PresentationControls
+            global
+            config={{ mass: 2, tension: 500 }}
+            snap={{ mass: 4, tension: 1500 }}
+            // rotation={[0, 0.3, 0]}
+            polar={[-Math.PI / 3, Math.PI / 3]}
+            azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+            cursor={false}
+          >
+            <ParticleCanvas
+              modelUrls={[
+                '/models/music/music.glb',
+                '/models/radio/radio.gltf',
+                '/models/ukulele/g.gltf',
+                '/models/mic/d.glb',
+              ]}
+            />
+          </PresentationControls>
           <Scroll html style={{ width: '100%', height: '100%' }}>
             <AllSections />
           </Scroll>
         </ScrollControls>
+
+        <Sparkles noise={0} count={500} speed={0.01} size={0.6} color={"#FFD2BE"} opacity={10} scale={[20, 100, 20]}></Sparkles>
+        <Sparkles noise={0} count={50} speed={0.01} size={10} color={"#FFF"} opacity={2} scale={[30, 100, 10]} ></Sparkles>
       </Canvas>
     </div>
   )
